0b2fbbde2fe5998f221b685909bcc9df8c471b5b
RanitManik <138437760+RanitManik@users.noreply.github.com>
2025-01-24 12:52:49 +0530

    #6 Dependency Injection using Spring Boot

---
+++ #6 Dependency Injection using Spring Boot/myApp/src/test/java/com/ranit/myapp/MyAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ranit.myapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyAppApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
